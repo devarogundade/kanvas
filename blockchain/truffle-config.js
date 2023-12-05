81a9655c31b8dc528e4f1b02226ad6e8d28d901a
devarogundade <devarogundade@gmail.com>
2023-12-05 11:41:29 +0100

    reployed

--- blockchain/truffle-config.js
+++ blockchain/truffle-config.js
@@ -12,7 +12,7 @@ module.exports = {
 
   networks: {
     fuji: {
-      provider: () => new HDWalletProvider(MNEMONIC, 'https://ava-testnet.public.blastapi.io/ext/bc/C/rpc'),
+      provider: () => new HDWalletProvider(MNEMONIC, 'https://api.avax-test.network/ext/bc/C/rpc'),
       network_id: 43113,
       confirmations: 2,
       timeoutBlocks: 9999999,
